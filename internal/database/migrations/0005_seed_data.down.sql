39a6b32bb0fdc63dc95259f7fcdf4122d2421e60
Milan Kovac <113798238+milan-kovac@users.noreply.github.com>
2025-11-26 18:43:38 +0100

    general small improvements

--- internal/database/migrations/0005_seed_data.down.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM players
-WHERE id IN (1,2,3,4);
-
-
-DELETE FROM tournaments
-WHERE id IN (1,2,3);
---
+++ internal/database/migrations/005_seed_data.down.sql
@@ -0,0 +1,5 @@
+DELETE FROM tournaments
+WHERE id IN (1,2,3,4,5,6,7,8,9,10);
+
+DELETE FROM players
+WHERE id IN (1,2,3,4,5,6,7,8,9,10);
